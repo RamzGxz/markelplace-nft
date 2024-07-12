94397db982dc387ca176a2fe20b9e99e616a333d
RamzGxz <ranzdaffa32@gmail.com>
2024-07-12 20:52:47 +0700

    add api

--- src/components/cards/bidCards/index.jsx
+++ src/components/cards/bidCards/index.jsx
@@ -1,28 +1,42 @@
 import { Heart } from '@phosphor-icons/react';
 import React, { useState } from 'react';
+import CountUp from 'react-countup';
 
-const BidsCard = ({ image, title, desc, price, bidPrice, likes }) => {
+const BidsCard = ({ tick, amount, unitPrice, inscriptionNumber, price }) => {
   const [loveClick, setLoveClick] = useState(false)
+  const numberFormat = (val) => {
+    const numb = new Intl.NumberFormat('en-EN', {
+      currency: 'USD',
+      currencyDisplay: 'narrowSymbol',
+      
+    }).format(val)
+    return numb
+  }
 
   return (
-    <div className='w-full p-5 rounded-md bg-primary/5 flex flex-col gap-5 group cursor-pointer relative'>
-      <div className='w-full lg:h-44 h-36 bg-gradient-to-r from-secondary to-accent rounded-md transform transition-transform duration-300 group-hover:scale-105 -z-0'></div>
-      <div className='flex flex-col gap-2'>
-        <div className='flex justify-between w-full'>
-          <p className='transition-colors duration-300 group-hover:text-accent lg:text-sm'>{title}</p>
-          <p className='lg:block hidden p-1 px-2 border border-accent rounded-md text-xs lg:text-accent text-text lg:bg-transparent bg-accent font-medium top-2 right-2'>{price} ETH </p>
-
-          <p className='lg:hidden absolute p-1 px-2 border border-accent rounded-md text-xs lg:text-accent text-text lg:bg-transparent bg-accent font-medium top-2 right-2'>{price} ETH </p>
+    <div className='w-full border border-secondary/40 p-1 rounded-md bg-primary/5 flex flex-col gap-5 group cursor-pointer relative'>
+      <div className='w-full lg:h-44 h-36 bg-gradient-to-r from-background via-secondary/15 rounded-md to-background  transform transition-transform duration-300 -z-0 flex justify-center items-center flex-col gap-3'>
+        <h1 className='text-4xl font-bold'>{numberFormat(amount)}</h1>
+        <div className='flex flex-col gap-1'>
+          <p className='text-xs'><span className='text-2xl font-bold text-accent'>{unitPrice}</span> sats/{tick}</p>
+          <p className='italic text-xs text-center'>$0.0287</p>
         </div>
-        <p className='text-sm italic text-start lg:block flex flex-col'>{desc} <span className='text-accent'>{bidPrice + ' '} ETH</span></p>
       </div>
-      <div className='flex justify-between items-center'>
-        <button className='px-2 p-1 text-xs border border-accent text-accent rounded-md font-semibold  transition-all duration-150 group-hover:bg-gradient-to-r group-hover:from-secondary group-hover:to-accent group-hover:text-text'>Place Bids</button>
+      <div className='absolute top-1 left-1 p-1 px-2 text-xs bg-secondary rounded-md'>{tick}</div>
+      <div className='flex gap-2 px-4 flex-col'>
+        <div className='flex justify-between w-full lg:flex-row flex-col gap-3'>
+          <p className='transition-colors duration-300 group-hover:text-accent lg:text-sm'>#{inscriptionNumber}</p>
+          <p className='p-1 px-2 border border-accent rounded-md text-xs ext-accent text-accent bg-transparent font-medium top-2 right-2'>{numberFormat(0.000 + price)} BTC</p>
+        </div>
+        <p className='text-sm italic text-start lg:block flex flex-col'>  <span className='text-accent'>{numberFormat((0.000 + price) * 57.626, 40)}</span></p>
+      </div>
+      <div className='flex justify-between items-center px-4 pb-4'>
+        <button className='px-2 p-1 text-xs border border-accent text-accent rounded-md font-semibold  transition-all duration-150 group-hover:bg-gradient-to-r group-hover:from-secondary group-hover:to-accent group-hover:text-text'>Buy now</button>
         <div className='flex items-center gap-1'>
           <button onClick={() => setLoveClick(!loveClick)} className='transition-transform duration-300 group-hover:scale-125'>
             <Heart size={18} color={loveClick ? '#ef4444' : '#e7eef6'} weight={loveClick ? 'fill' : 'regular'} />
           </button>
-          <p className='italic text-xs'>{likes}</p>
+          <p className='italic text-xs'>1</p>
         </div>
       </div>
     </div>
--- src/components/hotNft/index.jsx
+++ src/components/hotNft/index.jsx
@@ -1,20 +1,56 @@
 import { Heart } from '@phosphor-icons/react/dist/ssr';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BidsCard from '../cards/bidCards';
+import axios from 'axios';
 
 const HotNft = () => {
+  const [data, setData] = useState([])
+
+  const fetchData = async () => {
+    try {
+      const data = await axios.post('https://open-api.unisat.io/v3/market/brc20/auction/list', {
+        "filter": {
+          "nftType": "brc20",
+          "nftConfirm": true,
+          "isEnd": false,
+          "tick": "DBIX"
+        },
+        "sort": {
+          "unitPrice": 1
+        },
+        "start": 0,
+        "limit": 50,
+        "flash": false
+      }, {
+        headers: {
+          Authorization: `Bearer ${process.env.UNISAT_API_KEY}`
+        }
+      })
+      setData(data.data.data.list)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+    console.log(data)
+  }, [])
+
   return (
     <div className="w-full flex flex-col gap-16 py-16" id='bids'>
       <h1 className='text-center text-5xl font-semibold'>Hot Bids</h1>
       <div className='w-full grid lg:grid-cols-4 grid-cols-2 gap-5'>
-        <BidsCard bidPrice={0.25} desc={'Current Bids '} title={'Monkey Banana'} price={1.65} likes={120} />
-        <BidsCard bidPrice={0.40} desc={'Current Bids '} title={'Monkey flying'} price={1.8} likes={123} />
-        <BidsCard bidPrice={0.49} desc={'Current Bids '} title={'Robots Flex'} price={1.8} likes={123} />
-        <BidsCard bidPrice={0.49} desc={'Current Bids '} title={'Robots Flex'} price={1.8} likes={123} />
-        <BidsCard bidPrice={0.49} desc={'Current Bids '} title={'Robots Flex'} price={1.8} likes={123} />
-        <BidsCard bidPrice={0.49} desc={'Current Bids '} title={'Robots Flex'} price={1.8} likes={123} />
-        <BidsCard bidPrice={0.49} desc={'Current Bids '} title={'Robots Flex'} price={1.8} likes={123} />
-        <BidsCard bidPrice={0.49} desc={'Current Bids '} title={'Robots Flex'} price={1.8} likes={123} />
+        {data ? (
+          data.map((item, index) => {
+            return (
+              <BidsCard amount={item.amount} inscriptionNumber={item.inscriptionNumber} price={item.price} tick={item.tick} unitPrice={item.unitPrice} key={index}/>
+            )
+          })
+        ) : (
+          ''
+        )}
+
       </div>
     </div>
   );
